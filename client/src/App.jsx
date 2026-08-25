69c663d54e0c8c87db1525bace1cffaf835226ec
Laila <lailarw04@Lailas-MacBook-Air.local>
2026-08-25 11:13:04 -0500

    feat: scaffold client and server workspaces (Lecture 1)

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <main>
+        <h1>Inkwell</h1>
+        <p>A place for writers.. Under construction -- Lecture 1 of 25.</p>
+      </main>
+  );
+}
+export default App;
